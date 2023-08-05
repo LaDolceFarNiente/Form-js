61822a5cb035837b9e201b080a0f7da9ebc788f6
LaDolceFarNiente <rayimovadiana07@gmail.com>
2023-08-05 08:56:38 +0000

    12345

--- js/main.js
+++ js/main.js
@@ -3,12 +3,20 @@ const signUpBtn = document.querySelector('button.introBtn1')
 const createAcc = document.querySelector('.create_acc')
 let introArea1 = document.querySelector('.introduction1')
 let  introArea2 = document.querySelector('.introduction2')
-let   introTitle = document.querySelector('.text_title')
 let introBgColor = document.querySelector('.bgColor')
 let formArea = document.querySelector('.form_area')
 let formIn = document.querySelector('.form_area1')
 let formUp = document.querySelector('.form_area2')
 let form1 = document.querySelector('form.enter')
+
+let signUpName = document.querySelector('.signup_name')
+let signUpEmail = document.querySelector('.signup_email')
+let signUpPassword = document.querySelector('.signup_password')
+let signUpSubmit = document.querySelector('.signUp_submit')
+
+let signInEmail = document.querySelector('.signin_email')
+let signInPassword = document.querySelector('.signin_password')
+let signInSubmit = document.querySelector('.signIn_submit')
 
 function translate(e) {
     e.addEventListener('click', ()=> {
@@ -23,12 +31,55 @@ function translate(e) {
 translate(signUp)
 translate(signUpBtn)
 
+// VALIDATION
+let errors = document.querySelector('.error')
+const inputGroup = document.querySelector('.input_group')
+const formInField = document.querySelector('form.sign_in')
+// const signInInputs = document.querySelectorAll('form.sign_in>input')
+
+// signInInputs.forEach(input => {
+//   input.addEventListener('click', (inpElement)=> {
+//     validateForm(input)
+//   })
+// })
+
+
+
+formInField.addEventListener('submit', (event)=> {
+  event.preventDefault()
+  validateForm()
+})
+function validateForm() {
+  if(signInEmail.value.trim() == '') {
+    setError(signInEmail, 'Required!')
+  }else {
+    setSuccess(signInEmail)
+  }
+}
+
+function setError(element, message){
+  const parent = element.parentElement;
+  if(parent.classList.contains('success')){
+    parent.classList.remove('success')
+  }
+  parent.classList.add('warning')
+  errors.textContent = message
+}
+
+function setSuccess(element) {
+  const parent = element.parentElement;
+  if(parent.classList.contains('warning')){
+    parent.classList.remove('warning')
+  }
+  parent.classList.add('success')
+  errors.textContent= ''
+}
+
+
+
 let users = JSON.parse(localStorage.getItem('users'))?JSON.parse(localStorage.getItem('users')):[]
 // Sign Up user
-let signUpName = document.querySelector('.signup_name')
-let signUpEmail = document.querySelector('.signup_email')
-let signUpPassword = document.querySelector('.signup_password')
-let signUpSubmit = document.querySelector('.signUp_submit')
+
 
  function signUpUser (e) {
      let userObj = {
@@ -52,9 +103,7 @@ signUpSubmit.addEventListener('click',(e)=> {
   })
 
 //  log in
-let signInEmail = document.querySelector('.signin_email')
-let signInPassword = document.querySelector('.signin_password')
-let signInSubmit = document.querySelector('.signIn_submit')
+
 
 
 function signinUser(e) {
@@ -74,6 +123,40 @@ signInSubmit.addEventListener('click', (e)=> {
     signinUser()
 })
 
+
+
+
+// function validateName() {
+//   let inName = document.querySelector('.signin_email').value
+//   if(inName.length < 0) {
+//     errors.innerHTML = 'requred!'
+//     return false
+//   } else if (!inName.match(/^[A-Za-z]*\s{1}[A-Za-z]$/)) {
+//     errors.innerHTML = 'fill'
+//     return false
+//   }else {
+//     errors.innerHTML = 'valid'
+//     return true
+//   }
+// }
+
+// function setError(element, icon) {
+//   inputGroup.classList.add('.warning')
+// }
+
+// function validateInp(inp) {
+//   inp.addEventListener('click', ()=> {
+//     let inputs = inp.value
+//     if(inputs.length >= 3) {
+//       errors.innerHTML = 'requred'
+//       return false
+//     }else {
+//       errors.innerHTML = 'valid'
+//       return true
+//     }
+//   })
+// }
+// validateInp(document.querySelector('.signin_email').value)
 
 
 
